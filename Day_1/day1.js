b7ca95cc85787dd8757307e051c847c11e1ef14f
mhd101 <itzmohammadkhan@gmail.com>
2023-01-25 13:09:38 +0530

    Revised till Math Object

---
+++ Day_1/day1.js
@@ -0,0 +1,76 @@
+// Javascript Revision
+
+// var
+
+var x = 10;
+
+var x = 20;
+
+console.log(x);
+
+// let 
+
+
+let y = 20;
+
+// const y = 30; // error
+
+console.log(y);
+
+// const
+
+const z = 10;
+
+// const z= 20; // error
+
+console.log(z);
+
+
+// Math Objects
+
+const PI = Math.PI;
+
+console.log(PI)
+
+console.log(Math.round(PI)); // 3
+
+console.log(Math.round(9.81)); // round to 10
+
+console.log(Math.floor(PI)); // rounding down
+
+console.log(Math.ceil(PI));  // rounding up
+
+console.log(Math.min(1,2,3,4,5,-10)); // returns -10
+
+console.log(Math.max(1,2,3,4,5,-10)); // returns 5
+
+console.log(Math.random()) // returns random value between -0 to 0.999999
+
+console.log(Math.abs(-5)); // returns positive value
+
+console.log(Math.sqrt(3)); // returns square root value
+
+console.log(Math.pow(3,2)); // returns the power value
+
+console.log(Math.log(2)); // returns the log value
+
+console.log(Math.cos(0)); // returns trigonometric value
+
+
+// Strings
+
+let string =  ''; // empty string
+let string1 =  'Hello'; // single quote string
+let string2 =  "Hello"; // double quote string
+let string3 =  `Hello`; // backtick quote string
+
+
+let string4 = string1 + string2; // concatenation
+
+console.log(string4);
+
+
+
+
+
+
