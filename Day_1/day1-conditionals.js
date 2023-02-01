8bd8ca98be5ff25cf1afa7173c82097a2a8f3913
mhd101 <itzmohammadkhan@gmail.com>
2023-02-01 11:37:03 +0530

    Completed Level 1 Challenges of Day 1-Conditionals

---
+++ Day_1/day1-conditionals.js
@@ -0,0 +1,53 @@
+/* LEVEL 1 */
+
+// 1
+
+// const userAge = prompt('Enter Your Age: ');
+
+// if(userAge >= 18){
+//     console.log('You are old enough to drive.');
+// } else {
+//     console.log(`You are left with ${18-userAge} years to drive.`)
+// }
+
+// 2
+
+// const myAge = 20;
+// const yourAge = prompt('Enter your Age: ')
+
+// if (myAge > yourAge){
+//     console.log(`Your are ${myAge-yourAge} years younger than me.`)
+// } else {
+//     console.log(`Your are ${yourAge-myAge} years older than me.`)
+// }
+
+// 3
+
+let a = 4
+let b = 3
+
+if (a > b){
+    console.log('A is greater than B.');
+} else {
+    console.log('A is less than B')
+}
+
+(a > b) ? console.log('A is greater than B.') : console.log('A is less than B'); // ternary operator
+
+// 4
+
+// const number = prompt("Enter a Number: ")
+
+// if (number % 2 == 0){
+//     console.log(`${number} is an even number`)
+// } else {
+//     console.log(`${number} is an odd number`)
+// }
+
+
+/* LEVEL 2 */
+
+// 1
+
+
+
