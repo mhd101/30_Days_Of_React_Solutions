6830d2e1e2649d7027794b48cc21793dd3ba5279
mhd101 <itzmohammadkhan@gmail.com>
2023-02-06 19:26:58 +0530

    Completed Question 1&2 of Level 3 Challenges of Day 1-Objects

--- Day_1/day1-object.js
+++ Day_1/day1-object.js
@@ -14,8 +14,8 @@ dog.name = 'Rambo'
 dog.legs = 4
 dog.color = 'black'
 dog.age = 6
-dog.bark = function(){
-    return 'woof woof'
+dog.bark = function () {
+  return 'woof woof'
 }
 
 // 4
@@ -29,8 +29,8 @@ console.log(dog.bark())
 // 5
 
 dog.breed = 'German Shephard'
-dog.getDogInfo = function(){
-    return `Dog Name: ${dog.name}\nDog Age: ${dog.age}\nDog Color: ${dog.color}`
+dog.getDogInfo = function () {
+  return `Dog Name: ${dog.name}\nDog Age: ${dog.age}\nDog Color: ${dog.color}`
 }
 
 console.log(dog.getDogInfo())
@@ -39,67 +39,67 @@ console.log(dog.getDogInfo())
 /* LEVEL 2 */
 
 const users = {
-    Alex: {
-      email: 'alex@alex.com',
-      skills: ['HTML', 'CSS', 'JavaScript'],
-      age: 20,
-      isLoggedIn: false,
-      points: 30
-    },
-    Asab: {
-      email: 'asab@asab.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-      age: 25,
-      isLoggedIn: false,
-      points: 50
-    },
-    Brook: {
-      email: 'daniel@daniel.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-      age: 30,
-      isLoggedIn: true,
-      points: 50
-    },
-    Daniel: {
-      email: 'daniel@alex.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    },
-    John: {
-      email: 'john@john.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-      age: 20,
-      isLoggedIn: true,
-      points: 50
-    },
-    Thomas: {
-      email: 'thomas@thomas.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    },
-    Paul: {
-      email: 'paul@paul.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    }
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
   }
+}
 
 // 1
 
 let max = 0
 let skilledPerson;
 
-for(const u of Object.keys(users)){
-    if(users[u].skills.length > max){
-        max = users[u].skills.length
-        skilledPerson = u;
-    }
+for (const u of Object.keys(users)) {
+  if (users[u].skills.length > max) {
+    max = users[u].skills.length
+    skilledPerson = u;
+  }
 }
 console.log(skilledPerson)
 
@@ -109,12 +109,12 @@ console.log(skilledPerson)
 let loginCount = 0;
 let pointCount = 0;
 
-for(const u of Object.keys(users)){
-  if(users[u].isLoggedIn == true){
+for (const u of Object.keys(users)) {
+  if (users[u].isLoggedIn == true) {
     loginCount++
   }
-  
-  if (users[u].points >= 50){
+
+  if (users[u].points >= 50) {
     pointCount++
   }
 }
@@ -126,8 +126,8 @@ console.log(pointCount)
 
 console.log(users.Paul.skills.includes('MongoDB'))
 
-for(const u of Object.keys(users)){
-  if(users[u].skills.includes('MongoDB') && users[u].skills.includes('React') && users[u].skills.includes('Express') && users[u].skills.includes('Node')){
+for (const u of Object.keys(users)) {
+  if (users[u].skills.includes('MongoDB') && users[u].skills.includes('React') && users[u].skills.includes('Express') && users[u].skills.includes('Node')) {
     console.log(u)
   }
 }
@@ -159,7 +159,7 @@ console.log(Object.values(users))
 
 // 7
 
-for(const c of Object.keys(countriesData)){
+for (const c of Object.keys(countriesData)) {
   let printData = `Country Name: ${countriesData[c].name}\nCapital: ${countriesData[c].capital}\nPopulation Count: ${countriesData[c].population}\nLanguages: ${countriesData[c].languages}\n`
 
   console.log(printData)
@@ -167,4 +167,154 @@ for(const c of Object.keys(countriesData)){
 
 
 /* LEVEL 3 */
+
+// 1
+
+const personAccount = {
+  firstName: 'John',
+  lastName: 'Doe',
+  incomes: [{ category: 'Affiliate', income: 2000 }, { category: 'Course', income: 5000 }, { category: 'Salary', income: 10000 }],
+  expenses: [{ category: 'House Rent', expense: 2000 }, { category: 'Food', expense: 1000 }, { category: 'Other', expense: 2000 }],
+  totalIncome: function () {
+    let total = 0
+    for (const i of this.incomes) {
+      total = total + i.income
+    }
+    return total;
+  },
+  totalExpense: function () {
+    let total = 0
+    for (const e of this.expenses) {
+      total += e.expense;
+    }
+    return total;
+  },
+  addIncome: function (category, income) {
+    this.incomes.push({ category: category, income: income });
+  },
+  addExpense: function (category, expense) {
+    this.expenses.push({ category: category, expense: expense });
+  },
+  accountBalance: function () {
+    return this.totalIncome() - this.totalExpense();
+  },
+  accountInfo: function () {
+    return `Name: ${this.firstName + ' ' + this.lastName}\nAccount Balance: ${this.accountBalance()}`;
+  }
+
+}
+
+personAccount.addIncome('Youtube', 500)
+personAccount.addExpense('Medical', 1000)
+console.log(personAccount.totalIncome())
+console.log(personAccount.totalExpense())
+console.log(personAccount.accountInfo())
+
+// 2
+
+const usersDatabase = [
+  {
+    _id: 'ab12ex',
+    username: 'Alex',
+    email: 'alex@alex.com',
+    password: '123123',
+    createdAt: '08/01/2020 9:00 AM',
+    isLoggedIn: false,
+  },
+  {
+    _id: 'fg12cy',
+    username: 'Asab',
+    email: 'asab@asab.com',
+    password: '123456',
+    createdAt: '08/01/2020 9:30 AM',
+    isLoggedIn: true,
+  },
+  {
+    _id: 'zwf8md',
+    username: 'Brook',
+    email: 'brook@brook.com',
+    password: '123111',
+    createdAt: '08/01/2020 9:45 AM',
+    isLoggedIn: true,
+  },
+  {
+    _id: 'eefamr',
+    username: 'Martha',
+    email: 'martha@martha.com',
+    password: '123222',
+    createdAt: '08/01/2020 9:50 AM',
+    isLoggedIn: false,
+  },
+  {
+    _id: 'ghderc',
+    username: 'Thomas',
+    email: 'thomas@thomas.com',
+    password: '123333',
+    createdAt: '08/01/2020 10:00 AM',
+    isLoggedIn: false,
+  },
+]
+
+function signUp(username, email, password) {
+  let flag = false;
+  usersDatabase.forEach(x => {
+    if (x.username.toLowerCase() == username.toLowerCase()) {
+      flag = true
+    }
+  })
+  if (flag == true) {
+    console.log('User Already Exists!!')
+  } else {
+
+    // TimeStamp is created when the user signUp to know when his/her id has been created.
+    function createdAt() {
+      let now = new Date;
+    
+      let date = now.getDate()
+      let month = now.getMonth()
+      let year = now.getFullYear()
+      let hours = now.getHours()
+      let minutes = now.getMinutes();
+    
+      let dd = (date < 10) ? `0${date}` : `${date}`
+      let mm = (month < 10) ? `0${month}` : `${month}`
+      let yyyy = year
+    
+      let hh = (hours < 10) ? `0${hours}` : `${hours}`
+      let min = (minutes < 10) ? `0${minutes}` : `${minutes}`
+    
+      let ampm = (hh >= 12) ? 'PM' : 'AM'
+    
+      let createdAt = `${dd}/${mm}/${yyyy} ${hh}:${min} ${ampm}`
+    
+      return createdAt
+    }
+
+    // newUser Object
+    let newUser = {
+      id: 'cmis8f',
+      username: username,
+      email: email,
+      password: password,
+      createdAt: createdAt(),
+      isLoggedIn: false,
+    }
+
+    // Pushing newUser to usersDatabase array
+    usersDatabase.push(newUser)
+  }
+}
+
+
+// User is creating an account... 
+signUp('mhd','mhd101','123456')
+
+
+// Account Created!
+console.log(usersDatabase)
+
+
+
+
+
 
